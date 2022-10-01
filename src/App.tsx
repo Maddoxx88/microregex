9a2b8353b7737393ecddd052d17fc87f3813a794
localhoax <bhpndrsingh306@gmail.com>
2022-10-01 17:23:41 +0530

    Initial commit for Chakra UI, added react router and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./pages/home";
+import Navbar from "./components/navbar";
+import { Box } from "@chakra-ui/react";
+
+import theme from "./theme";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ChakraProvider theme={theme}>
+			<Box minH='100vh'>
+				<Navbar />
+
+				<Box minH='calc(100vh - 60px)'>
+					<Router>
+						<Routes>
+							<Route path='/' element={<HomePage />} />
+						</Routes>
+					</Router>
+				</Box>
+			</Box>
+		</ChakraProvider>
+	);
 }
 
 export default App;
